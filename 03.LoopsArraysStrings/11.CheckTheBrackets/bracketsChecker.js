5efd67b14fc546a2c34ec3a56f373ffd48dce598
mitko100 <dimitar.radenkov1@gmai.com>
2014-10-05 17:31:35 +0300

    problem 11 and problem 12 - implemented.

---
+++ 03.LoopsArraysStrings/11.CheckTheBrackets/bracketsChecker.js
@@ -0,0 +1,39 @@
+﻿"use strict";
+
+var arr1 = ['(', '(', 'a', '+', 'b', ')', '/', '5', '–', 'd', ')'];
+var arr2 = [')', '(', 'a', '+', 'b' ,')', ')'];
+var arr3 = ['(', 'b', '*', '(', 'c', '+', 'd', '*', '2',
+            '/', '(', '2', '+', '(', '12', '–', 'c', '/', '(',
+            'a', '+', '3', ')', ')', ')', ')'];
+
+console.log(checkBrackets(arr1));
+console.log(checkBrackets(arr2));
+console.log(checkBrackets(arr3));
+
+function checkBrackets(charArray) {
+    var positiveAnswer = "correct";
+    var negativeAnswer = "inccorect";
+
+    var open = '(';
+    var close = ')';
+
+    var counter = 0;
+
+    for (var i in charArray) {
+        var char = charArray[i];
+
+        if (char == open) {
+            counter++;
+        }
+        else if (char == close) {
+            counter--;
+        }
+    }
+
+    if (counter == 0) {
+        return positiveAnswer;
+    }
+    else {
+        return negativeAnswer;
+    }
+}
---
+++ 03.LoopsArraysStrings/12.SubstringCount/substringSearch.js
@@ -0,0 +1,33 @@
+﻿"use strict";
+
+var keywordText1 = ["in",
+    "We are living in a yellow submarine. We don't have anything else." +
+    "Inside the submarine is very tight. So we are drinking all the day." +
+    " We will move out of it in 5 days."];
+
+var keywordText2 = ["your",
+    "No one heard a single word you said. " +
+    "They should have seen it in your eyes. What was going around your head."];
+
+var keywordText3 = ["but",
+    "But you were living in another world tryin' to get your message through."];
+
+console.log(countSubstringOccur(keywordText1));
+console.log(countSubstringOccur(keywordText2));
+console.log(countSubstringOccur(keywordText3));
+
+function countSubstringOccur(value) {
+    var keyWord = value[0].toLowerCase();
+    var text = value[1].toLowerCase();
+
+    var counter = 0;
+
+    var pos = text.indexOf(keyWord, 0);     
+    while (pos != -1) {
+        counter++;
+
+        pos = text.indexOf(keyWord, pos + 1);
+    }
+    
+    return counter;
+}
