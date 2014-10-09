372fe06293d6b33bced860f824d209013e803417
mitko100 <dimitar.radenkov1@gmai.com>
2014-10-09 22:58:12 +0300

    problem 17 and 18 - implemented.

---
+++ 03.LoopsArraysStrings/17.ExtractElementContent/contentExtracter.js
@@ -0,0 +1,22 @@
+﻿"use strict";
+
+var htmlContent = "<p>Hello</p><a href='http://w3c.org'>W3C</a>";
+extractContent(htmlContent);
+
+function extractContent(value) {
+    
+    var currentPos = 0;
+    var totalContent = "";
+
+    while (currentPos != -1) {
+        var openPos = value.indexOf(">", currentPos);
+        var closePos = value.indexOf("</", openPos + 1);
+        var content = value.substring(openPos + 1, closePos);
+        totalContent += context;
+
+        //find new open tag
+        currentPos = value.indexOf(">", closePos + 1) + 1;
+    }
+
+    console.log(totalContent);
+}
---
+++ 03.LoopsArraysStrings/18.ReplaceATag/aTagReplacer.js
@@ -0,0 +1,18 @@
+﻿"use strict";
+
+var html = [""
+,"<ul>"
+    ,"<li>"
+        ,"<a href=http://softuni.bg>SoftUni</a>"
+    ,"</li>"
+,"</ul>"].join("");
+
+var result = replaceATag(html);
+console.log(result);
+
+function replaceATag(value) {
+    value = value.replace("<a", "[URL");
+    value = value.replace("</a>", "[/URL]");
+
+    return value;
+}
