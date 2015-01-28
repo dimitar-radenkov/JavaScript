840f6ece7eb1ea9e7e5cad04e48f1cea304838be
mitko100 <dimitar.radenkov1@gmai.com>
2015-01-28 22:05:09 +0200

    angular intro

---
+++ 04.AngularJS/01.Intro/01.NG-SRC/scripts.js
@@ -0,0 +1,16 @@
+﻿var app = angular.module('01.App', []);
+
+app.controller('studentController', function ($scope) {
+    var obj =
+        {
+            "name": "Pesho",
+            "photo": "http://www.nakov.com/wp-content/uploads/2014/05/SoftUni-Logo.png",
+            "grade": 5,
+            "school": "High School of Mathematics",
+            "teacher": "Gichka Pesheva",
+        };
+
+    $scope.student = obj;
+});
+
+
---
+++ 04.AngularJS/01.Intro/03.Tiger/app.js
@@ -0,0 +1,14 @@
+﻿var app = angular.module('tigerApp', []);
+
+app.controller('tigerController', function ($scope) {
+
+    var obj =
+    {
+        'Name': 'Pesho',
+        'Born': 'Asia',
+        'BirthDate': 2006,
+        'Live': 'Sofia Zoo',
+    };
+
+    $scope.tiger = obj;
+});
